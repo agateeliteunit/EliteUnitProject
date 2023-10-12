0fcd80dbebdf7d90053971a39695175182a98a0e
Bayu Tantra Bramandhita <73697375+bayutb123@users.noreply.github.com>
2023-10-12 10:09:12 +0700

    Reset / Prepare World UI Controller

--- Assets/EliteUnit/Scripts/Development/World/World/WorldUIController.cs
+++ Assets/EliteUnit/Scripts/Development/World/World/WorldUIController.cs
@@ -11,38 +11,5 @@ public class WorldUIController : MonoBehaviour
     // [SerializeField] private TMP_Text _currentGrenades;
     // [SerializeField] private GameObject _missionFinishedCanvas;
     // [SerializeField] private GameObject _GUICanvas;
-    // Start is called before the first frame update
     
-    
-    void Start()
-    {
-        // _missionFinishedCanvas.SetActive(false);
-    }
-    
-    public void UpdateTimeLeft(float timeLeft)
-    {
-        // _timeLeftText.text = "Time left: " + Mathf.Round(timeLeft);
-    }
-    
-	public void UpdateAmmo(int ammo)
-    {
-        // _currentAmmo.text = ammo.ToString();
-    }
-
-	public void UpdateHealth(int health)
-    {
-        // _currentHealth.text = "Health: " + health;
-    }
-
-	public void UpdateGrenades(int grenades)
-    {
-        // _currentGrenades.text = grenades.ToString();	
-    }
-    
-    public void MissionFinished(int timeLeft)
-    {
-        
-        // _missionFinishedCanvas.SetActive(true);
-        // _GUICanvas.SetActive(false);
-    }
 }
