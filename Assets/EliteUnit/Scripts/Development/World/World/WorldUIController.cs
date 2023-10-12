3069b5393455e697a678f03093356846dc0c0bfb
Samueladi5 <88497785+Samueladi5@users.noreply.github.com>
2023-10-12 11:41:53 +0700

    Input Asset UI & Function Play Button

--- Assets/EliteUnit/Scripts/Development/World/World/WorldUIController.cs
+++ Assets/EliteUnit/Scripts/Development/World/World/WorldUIController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class WorldUIController : MonoBehaviour
 {
@@ -13,7 +14,10 @@ public class WorldUIController : MonoBehaviour
     // [SerializeField] private GameObject _GUICanvas;
     // Start is called before the first frame update
     
-    
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Stage 1");
+    }
     void Start()
     {
         // _missionFinishedCanvas.SetActive(false);
