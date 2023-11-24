69cf824d16859a76e6028a3380c36eea0bc269c6
NGM7901 <20210410700059@student.umj.ac.id>
2023-11-24 17:03:23 +0700

    main menu & menu level

--- Assets/EliteUnit/Level/Scripts/Development/Cutscene/Cutscene1.cs
+++ Assets/EliteUnit/Level/Scripts/Development/Cutscene/Cutscene1.cs
@@ -27,7 +27,7 @@ public class Cutscene1 : MonoBehaviour
 
     IEnumerator cekCutscene()
     {
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(4f);
 
         while (videoCutscene.isPlaying)
         {
